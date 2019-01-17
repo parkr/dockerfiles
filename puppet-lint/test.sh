220923a5d086358aceb2deae9f724f80c43522d5
Parker Moore <parkrmoore@gmail.com>
2019-01-17 17:38:41 -0500

    Make puppet-lint work well with our new Makefile

---
+++ puppet-lint/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -L "$0" ]; then
+    root=$(cd "$(dirname "$(readlink "$0")")" && pwd)
+else
+    root=$(cd "$(dirname "$0")" && pwd)
+fi
+
+TAG="$1"
+test -z "$1" && {
+    echo "usage: $0 <FULL_TAG>"
+    exit 1
+}
+
+set -x
+
+docker run -it --rm "$TAG" -h
