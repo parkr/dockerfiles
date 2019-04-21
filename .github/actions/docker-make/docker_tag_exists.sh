c37376cb3c1ab77dfb8b489822f7de189bb3b3d1
Parker Moore <parkrmoore@gmail.com>
2019-04-21 11:44:44 -0400

    docker_tag_exists.sh: exit 0 when it exists, and run always if VERSION is 'latest'

--- .github/actions/docker-make/docker_tag_exists.sh
+++ .github/actions/docker-make/docker_tag_exists.sh
@@ -2,6 +2,11 @@
 
 set -eu
 
+if [ $# -eq 0 ]; then
+  echo "usage: $0 <project> -- <command>"
+  exit 1
+fi
+
 PROJECT_NAME="$1"
 VERSION=$(cat "$PROJECT_NAME"/VERSION)
 shift
@@ -14,14 +19,30 @@ function docker_tag_exists() {
     test $EXISTS = true
 }
 
+function run_command() {
+  if [ "$1" == "--" ]; then
+    shift
+  fi
+  exec sh -c "$*"
+}
+
+if [ "${VERSION}" = "latest" ]; then
+  run_command "$@"
+  exit $?
+fi
+
 if docker_tag_exists "parkr/${PROJECT_NAME}" "${VERSION}"; then
     echo "parkr/${PROJECT_NAME}:${VERSION}" exists on Docker Hub.
-    exit 78
-else
-    echo "parkr/${PROJECT_NAME}:${VERSION}" does not exist on Docker Hub.
-    if [ "$1" == "--" ]; then
-        shift
-        exec sh -c "$*"
-    fi
+    # Normally, we would exit 78 and the GitHub Actions UI would show that
+    # this is "declined" and the other targets would build. Unfortunately,
+    # due to a limitation in GitHub Actions, parallel builds are still
+    # canceled when this one is canceled, so we return a happy code instead
+    # to know that we've satisfied the request of the Action.
+    # exit 78
     exit 0
+else
+  echo "parkr/${PROJECT_NAME}:${VERSION}" does not exist on Docker Hub.
 fi
+
+run_command "$@"
+exit $?
