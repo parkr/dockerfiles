f3f9c9b18fd2e86a021904240b9da584fb446eb4
Parker Moore <parkrmoore@gmail.com>
2019-10-17 16:23:50 -0400

    Try using a GITHUB_TOKEN for GPR push

--- .github/actions/github-pkg-login/entrypoint.sh
+++ .github/actions/github-pkg-login/entrypoint.sh
@@ -2,4 +2,4 @@
 
 DOCKER_REGISTRY_URL="docker.pkg.github.com"
 echo "Logging into $DOCKER_REGISTRY_URL..."
-echo "$GPR_PASSWORD" | docker login -u "$GPR_USERNAME" --password-stdin "$DOCKER_REGISTRY_URL"
+echo "$GITHUB_TOKEN" | docker login -u "$GPR_USERNAME" --password-stdin "$DOCKER_REGISTRY_URL"
