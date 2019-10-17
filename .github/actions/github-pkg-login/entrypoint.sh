364ca6500c744c10d3c37a3586188ca2f7c0fa9e
Parker Moore <parkrmoore@gmail.com>
2019-10-17 16:33:40 -0400

    Remove github-pkg-login action

--- .github/actions/github-pkg-login/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -lu
-
-DOCKER_REGISTRY_URL="docker.pkg.github.com"
-echo "Logging into $DOCKER_REGISTRY_URL..."
-echo "$GITHUB_TOKEN" | docker login -u "$GPR_USERNAME" --password-stdin "$DOCKER_REGISTRY_URL"
