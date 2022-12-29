e12aafcfc1c50f9bcde2c368dd51f44271d3c698
Parker Moore <parkr@users.noreply.github.com>
2022-12-29 12:26:30 +0100

    airconnect: use --fail on curl call for latest version

--- airconnect/latest_version.sh
+++ airconnect/latest_version.sh
@@ -8,6 +8,6 @@ current_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 version_file_path="${current_dir}/VERSION"
 echo "$version_file_path"
 
-curl --silent https://api.github.com/repos/philippe44/AirConnect/releases/latest | jq -r .tag_name > "$version_file_path"
+curl --silent --fail https://api.github.com/repos/philippe44/AirConnect/releases/latest | jq -r .tag_name > "$version_file_path"
 
 cat "$version_file_path"
