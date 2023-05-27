097ce167296cd93f9e0f326b054db7f8f75a7cef
Parker Moore <parkrmoore@gmail.com>
2023-05-27 16:10:45 -0700

    Use teslamate-abrp releases

--- teslamate-abrp/latest_version.sh
+++ teslamate-abrp/latest_version.sh
@@ -8,7 +8,6 @@ current_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 version_file_path="${current_dir}/VERSION"
 echo "$version_file_path"
 
-# 'rclone vx.y.z'
-curl --silent https://api.github.com/repos/fetzu/teslamate-abrp/commits/main | jq -r .sha > "$version_file_path"
+curl --silent --fail https://api.github.com/repos/fetzu/teslamate-abrp/releases/latest | jq -r .tag_name > "$version_file_path"
 
 cat "$version_file_path"
