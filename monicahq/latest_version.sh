f1b80fa0b75fe8e452d66427a026d707eeefb647
Parker Moore <parkrmoore@gmail.com>
2024-05-25 20:47:45 -0700

    page_size=100 for monicahq tags

--- monicahq/latest_version.sh
+++ monicahq/latest_version.sh
@@ -9,7 +9,7 @@ current_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 
 curl \
   --silent \
-  https://hub.docker.com/v2/repositories/library/monica/tags/ > output
+  https://hub.docker.com/v2/repositories/library/monica/tags?page_size=100 > output
 (jq -r .results[].name output | grep \\-apache | head -n 1 | cut -d- -f1 > "${current_dir}/VERSION") || {
     cat output
     echo "FAILURE"
