41c1b7e1e8dc325d6db436ba102f82b47eb92bdc
Parker Moore <parkrmoore@gmail.com>
2024-10-18 09:46:06 -0700

    Add yt-dlp, using platform-specific binary builds

---
+++ yt-dlp/README.md
@@ -0,0 +1,9 @@
+## yt-dlp
+
+Repackaged this software into a Docker container. Use it thusly:
+
+```text
+docker run --rm -v "$(pwd)":/yt-dlp parkr/yt-dlp:TAG <args>
+```
+
+Latest TAG can be found on [Docker Hub](https://hub.docker.com/r/parkr/yt-dlp/tags).
---
+++ yt-dlp/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -L "$0" ]; then
+    root=$(cd "$(dirname "$(readlink "$0")")" && pwd)
+else
+    root=$(cd "$(dirname "$0")" && pwd)
+fi
+
+TAG="$1"
+test -z "$1" && {
+    echo "usage: $0 <TAG>"
+    exit 1
+}
+
+set -x
+docker run --rm -v "$(pwd)":/yt-dlp "$TAG" "https://soundcloud.com/royalty-free-music-track/this-8-bit-game-music-royalty"
